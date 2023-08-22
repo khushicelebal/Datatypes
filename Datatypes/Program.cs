212fb8fcd227aedb756d0c61364170284207e5f7
khush <khush@LAPTOP-15K03B8M>
2023-08-22 15:28:11 +0530

    Add project files.

---
+++ Datatypes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datatypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 100000;
+            Console.WriteLine(myInt);
+
+            long myLong = 15000000000L;
+            Console.WriteLine(myLong);
+
+            float myFloat = 5.17F;
+            Console.WriteLine(myFloat);
+
+            double myDouble = 18.08D;
+            Console.WriteLine(myDouble);
+
+            bool isCSharpFun = true;
+            bool isMaggiHealthy = false;
+            Console.WriteLine(isCSharpFun);   
+            Console.WriteLine(isMaggiHealthy);
+
+            char myChar = 'A';
+            Console.WriteLine(myChar);
+
+            string greeting = "Good Morning";
+            Console.WriteLine(greeting);
+        }
+    }
+}
